6dc1a8b2d190c4099eaa759e3e7ce2c390fa7094
abdulrahman moussa <a.moussa.intern@wecodeforyou.io>
2025-05-01 20:43:16 +0300

    test remove query key

--- packages/client-fetch/src/utils.ts
+++ packages/client-fetch/src/utils.ts
@@ -359,7 +359,13 @@ export const createQsQuerySerializer = <T = unknown>({
   // Return the serializer function
   const querySerializer = (queryParams: T) => {
     if (!queryParams || typeof queryParams !== 'object') return '';
-    return qs.stringify(queryParams as Record<string, any>, qsOptions);
+    console.log(queryParams, 'queryParams');
+    let finalQueryParams = queryParams;
+    if ((queryParams as any)?.query?.query) {
+      finalQueryParams = (queryParams as any)?.query;
+    }
+
+    return qs.stringify(finalQueryParams as Record<string, any>, qsOptions);
   };
 
   return querySerializer;
